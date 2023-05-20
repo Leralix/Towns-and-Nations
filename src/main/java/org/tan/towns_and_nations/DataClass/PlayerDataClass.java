cff40dd31a1ea98afde75be818865933e16e2176
Leralix <leralix98@gmail.com>
2023-05-20 16:55:15 +0200

    Données de ville sur les joueurs

--- src/main/java/org/tan/towns_and_nations/DataClass/PlayerDataClass.java
+++ src/main/java/org/tan/towns_and_nations/DataClass/PlayerDataClass.java
@@ -7,11 +7,13 @@ public class PlayerDataClass {
     private String UUID;
     private String PlayerName;
     private int Balance;
+    private int TownId;
 
     public PlayerDataClass(Player player) {
         this.UUID = player.getUniqueId().toString();
         this.PlayerName = player.getName();
         this.Balance = 0;
+        this.TownId = -1;
     }
 
 
@@ -34,6 +36,17 @@ public class PlayerDataClass {
     public void setBalance(int balance) {
         this.Balance = balance;
     }
+
+    public int getTownId(){
+        return this.TownId;
+    }
+    public boolean haveTown(){
+        return !(this.TownId == -1);
+    }
+    public void setTownId(int newTownId){
+        this.TownId = newTownId;
+    }
+
 
     public void addToBalance(int money) {
         this.Balance = this.Balance + money;
--- src/main/java/org/tan/towns_and_nations/DataClass/TownDataClass.java
+++ src/main/java/org/tan/towns_and_nations/DataClass/TownDataClass.java
@@ -3,16 +3,26 @@ package org.tan.towns_and_nations.DataClass;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TownDataClass {
 
     private String UuidLeader;
     private String TownName;
     private ItemStack townIcon = null;
 
+    private HashMap<String, ArrayList<String>> townPlayerList = new HashMap<>();
+
     public TownDataClass( String uuidLeader, String townName){
         this.UuidLeader = uuidLeader;
         this.TownName = townName;
         this.townIcon = null;
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add(uuidLeader);
+        this.townPlayerList.put("Leader", list);
+
     }
 
 
