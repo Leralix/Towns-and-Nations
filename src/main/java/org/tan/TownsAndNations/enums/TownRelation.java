2f9edd64cca12503c77da2d5556520d6233d7938
Leralix <leralix98@gmail.com>
2023-09-15 13:43:17 +0200

    Hunting the non-localized Strings

--- src/main/java/org/tan/TownsAndNations/enums/TownRelation.java
+++ src/main/java/org/tan/TownsAndNations/enums/TownRelation.java
@@ -1,14 +1,16 @@
 package org.tan.TownsAndNations.enums;
 
 import org.bukkit.ChatColor;
+import org.tan.TownsAndNations.Lang.Lang;
 
 public enum TownRelation {
 
-    CITY("City", ChatColor.GREEN, Boolean.FALSE, Boolean.FALSE),
-    ALLIANCE("Alliance", ChatColor.BLUE, Boolean.TRUE, Boolean.FALSE),
-    NON_AGGRESSION("Non-Aggression", ChatColor.DARK_AQUA, Boolean.TRUE, Boolean.FALSE),
-    EMBARGO("Embargo", ChatColor.GOLD, Boolean.FALSE, Boolean.TRUE),
-    WAR("War", ChatColor.RED, Boolean.FALSE, Boolean.TRUE);
+    NEUTRAL(Lang.RELATION_NEUTRAL_NAME.getTranslation(), ChatColor.WHITE, Boolean.FALSE, Boolean.FALSE),
+    CITY(Lang.RELATION_CITY_NAME.getTranslation(), ChatColor.GREEN, Boolean.FALSE, Boolean.FALSE),
+    ALLIANCE(Lang.RELATION_ALLIANCE_NAME.getTranslation(), ChatColor.BLUE, Boolean.TRUE, Boolean.FALSE),
+    NON_AGGRESSION(Lang.RELATION_NON_AGGRESSION_NAME.getTranslation(), ChatColor.DARK_AQUA, Boolean.TRUE, Boolean.FALSE),
+    EMBARGO(Lang.RELATION_EMBARGO_NAME.getTranslation(), ChatColor.GOLD, Boolean.FALSE, Boolean.TRUE),
+    WAR(Lang.RELATION_WAR_NAME.getTranslation(), ChatColor.RED, Boolean.FALSE, Boolean.TRUE);
 
     private final String name;
     private final ChatColor color;
