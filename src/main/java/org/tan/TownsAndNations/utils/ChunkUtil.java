049af1a9bc56683ca04a02b6928f99f8e4d550e2
Leralix <78090544+Leralix@users.noreply.github.com>
2024-03-30 13:50:17 +0100

    v0.6.3

--- src/main/java/org/tan/TownsAndNations/utils/ChunkUtil.java
+++ src/main/java/org/tan/TownsAndNations/utils/ChunkUtil.java
@@ -17,41 +17,8 @@ public class ChunkUtil {
 
 
     public static void claimChunkForRegion(Player player) {
-
-        //No town
-        PlayerData playerStat = PlayerDataStorage.get(player.getUniqueId().toString());
-        if(!playerStat.haveTown()){
-            player.sendMessage(getTANString() + Lang.PLAYER_NO_TOWN.get());
-            return;
-        }
-
-        //No region
-        TownData townData = TownDataStorage.get(player);
-        if(!townData.haveRegion()){
-            player.sendMessage(getTANString() + Lang.TOWN_NO_REGION.get());
-            return;
-        }
-
-        RegionData regionData = townData.getRegion();
-
-        //Not leader of the region
-        if(!regionData.isPlayerLeader(playerStat)){
-            player.sendMessage(getTANString() + Lang.PLAYER_NOT_LEADER_OF_REGION.get());
-            return;
-        }
-        int cost = ConfigUtil.getCustomConfig("config.yml").getInt("CostOfRegionChunk",5);
-
-        if(regionData.getBalance() < cost){
-            player.sendMessage(getTANString() + Lang.REGION_NOT_ENOUGH_MONEY_EXTENDED.get(cost - regionData.getBalance()));
-            return;
-        }
-
-
         Chunk chunkToClaim = player.getLocation().getChunk();
-        regionData.removeBalance(cost);
-        NewClaimedChunkStorage.claimRegionChunk(chunkToClaim, regionData.getID());
-        player.sendMessage(getTANString() + Lang.CHUNK_CLAIMED_SUCCESS_REGION.get());
-
+        claimChunkForTown(player, chunkToClaim);
     }
     public static void claimChunkForRegion(Player player, Chunk chunk) {
 
@@ -91,50 +58,8 @@ public class ChunkUtil {
 
     public static void claimChunkForTown(Player player) {
 
-        //No town
-        PlayerData playerStat = PlayerDataStorage.get(player.getUniqueId().toString());
-        if(!playerStat.haveTown()){
-            player.sendMessage(getTANString() + Lang.PLAYER_NO_TOWN.get());
-            return;
-        }
-
-        //No permission
-        TownData townData = TownDataStorage.get(player);
-        if(!playerStat.hasPermission(TownRolePermission.CLAIM_CHUNK)){
-            player.sendMessage(getTANString() + Lang.PLAYER_NO_PERMISSION.get());
-        }
-
-        //Chunk limit reached
-        if(!townData.canClaimMoreChunk()){
-            player.sendMessage(getTANString() + Lang.MAX_CHUNK_LIMIT_REACHED.get());
-            return;
-        }
-        boolean isRegionClaimed = false;
-
-        //Chunk already claimed by the town
         Chunk chunkToClaim = player.getLocation().getChunk();
-        if(NewClaimedChunkStorage.isChunkClaimed(chunkToClaim)){
-            //If chunk belongs to the region in which the town is, then the town can get the chunk
-            if(NewClaimedChunkStorage.isChunkClaimedByTownRegion(townData,chunkToClaim)){
-                isRegionClaimed = true;
-            }
-            else{
-                player.sendMessage(getTANString() + Lang.CHUNK_ALREADY_CLAIMED_WARNING.get(NewClaimedChunkStorage.getChunkOwnerName(chunkToClaim)));
-                return;
-            }
-        }
-
-        if(townData.getNumberOfClaimedChunk() != 0 &&
-                !NewClaimedChunkStorage.isAdjacentChunkClaimedBySameTown(chunkToClaim,townData.getID()) &&
-                ConfigUtil.getCustomConfig("config.yml").getBoolean("AllowNonAdjacentChunks",false))
-        {
-            player.sendMessage(getTANString() + Lang.CHUNK_NOT_ADJACENT.get());
-            return;
-        }
-
-        TownClaim(isRegionClaimed, chunkToClaim, townData, player);
-
-
+        claimChunkForTown(player, chunkToClaim);
     }
     public static void claimChunkForTown(Player player, Chunk chunkToClaim) {
 
