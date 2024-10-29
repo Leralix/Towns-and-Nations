50c1f97d69cd4e2d27bd0b600205bb771ab7c4f0
Leralix <78090544+Leralix@users.noreply.github.com>
2024-10-29 22:25:50 +0100

    v0.11.3

--- src/main/java/org/leralix/tan/storage/ClaimBlacklistStorage.java
+++ src/main/java/org/leralix/tan/storage/ClaimBlacklistStorage.java
@@ -2,22 +2,26 @@ package org.leralix.tan.storage;
 
 import org.bukkit.Chunk;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.leralix.tan.utils.config.ConfigTag;
 import org.leralix.tan.utils.config.ConfigUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class ClaimBlacklistStorage {
+
+    private ClaimBlacklistStorage() {
+        throw new IllegalStateException("Utility class");
+    }
 
     private static List<BlacklistInstance> blacklist;
 
     public static void init() {
         blacklist = new ArrayList<>();
         FileConfiguration config = ConfigUtil.getCustomConfig(ConfigTag.MAIN);
-        for (Object item : config.getList("claimBlacklist")) {
+        for (Object item : config.getList("claimBlacklist", Collections.emptyList())){
             if (item instanceof Map<?, ?> map) {
                 String name = (String) map.get("name");
                 List<Integer> coordinates = (List<Integer>) map.get("coordinate");
@@ -26,7 +30,6 @@ public class ClaimBlacklistStorage {
                 }
             }
         }
-        System.out.println("ClaimBlacklistStorage initialized with " + blacklist.size() + " blacklisted areas.");
     }
 
     public static boolean cannotBeClaimed(Chunk chunk) {
