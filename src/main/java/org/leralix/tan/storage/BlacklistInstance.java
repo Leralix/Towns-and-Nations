1cae2d1da5f3b82e087c2ba890b502ccf5a62fcb
Leralix <78090544+Leralix@users.noreply.github.com>
2025-02-23 23:27:19 +0100

    Fixing chunk blacklist registering

--- src/main/java/org/leralix/tan/storage/BlacklistInstance.java
+++ src/main/java/org/leralix/tan/storage/BlacklistInstance.java
@@ -6,20 +6,26 @@ import java.util.List;
 
 public class BlacklistInstance {
     String worldName;
-    int x1;
-    int z1;
-    int x2;
-    int z2;
+    int xMin;
+    int zMin;
+    int xMax;
+    int zMax;
 
     public BlacklistInstance(String name, List<Integer> coordinates) {
         worldName = name;
-        x1 = coordinates.get(0);
-        z1 = coordinates.get(1);
-        x2 = coordinates.get(2);
-        z2 = coordinates.get(3);
+
+        int x1 = coordinates.get(0);
+        int z1 = coordinates.get(1);
+        int x2 = coordinates.get(2);
+        int z2 = coordinates.get(3);
+
+        xMin = Math.min(x1, x2);
+        xMax = Math.max(x1, x2);
+        zMin = Math.min(z1, z2);
+        zMax = Math.max(z1, z2);
     }
 
     public boolean isChunkInArea(Chunk chunk) {
-        return chunk.getWorld().getName().equals(worldName) && chunk.getX() >= x1 && chunk.getX() <= x2 && chunk.getZ() >= z1 && chunk.getZ() <= z2;
+        return chunk.getWorld().getName().equals(worldName) && chunk.getX() >= xMin && chunk.getX() <= xMax && chunk.getZ() >= zMin && chunk.getZ() <= zMax;
     }
 }
