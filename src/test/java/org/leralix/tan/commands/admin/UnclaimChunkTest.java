da201613dac983059893a6bbf23f14fc9c781836
Leralix <78090544+Leralix@users.noreply.github.com>
2025-04-04 00:04:18 +0200

    more UnclaimChunk UT

--- src/test/java/org/leralix/tan/commands/admin/UnclaimChunkTest.java
+++ src/test/java/org/leralix/tan/commands/admin/UnclaimChunkTest.java
@@ -21,6 +21,8 @@ import org.mockito.Mockito;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -28,13 +30,15 @@ public class UnclaimChunkTest {
 
     public static CommandSender sender;
     public static Chunk chunkPosition;
+    public static Chunk otherChunkPosition;
     public static World world;
+    public static TownData townData;
 
     @BeforeAll
     static void initialise(){
         Factory.initializeConfigs();
 
-        TownData townData = TownDataStorage.getInstance().newTown("Cool town");
+        townData = TownDataStorage.getInstance().newTown("Cool town");
 
 
         world = Mockito.mock(World.class);
@@ -45,22 +49,42 @@ public class UnclaimChunkTest {
         when(chunkPosition.getX()).thenReturn(0);
         when(chunkPosition.getZ()).thenReturn(0);
 
+        otherChunkPosition = Mockito.mock(Chunk.class);
+        when(otherChunkPosition.getWorld()).thenReturn(world);
+        when(otherChunkPosition.getX()).thenReturn(-1);
+        when(otherChunkPosition.getZ()).thenReturn(-1);
+
         when(world.getChunkAt(any(Location.class))).thenReturn(chunkPosition);
 
-        NewClaimedChunkStorage.getInstance().claimTownChunk(chunkPosition, townData.getID());
         Factory.initializeConfigs();
+
+        assertFalse(NewClaimedChunkStorage.getInstance().isChunkClaimed(chunkPosition));
+        NewClaimedChunkStorage.getInstance().claimTownChunk(chunkPosition, townData.getID());
+        assertTrue(NewClaimedChunkStorage.getInstance().isChunkClaimed(chunkPosition));
     }
 
     @Test
     void standardUse() {
-
         Player player = Factory.getRandomPlayer();
         when(player.getLocation()).thenReturn(new Location(world, 0, 0, 0));
-
 
         UnclaimAdminCommand unclaim = new UnclaimAdminCommand();
         unclaim.perform(player, new String[]{"unclaim"});
 
+        assertFalse(NewClaimedChunkStorage.getInstance().isChunkClaimed(chunkPosition));
+    }
 
+    @Test
+    void playerOnAnotherChunk() {
+        NewClaimedChunkStorage.getInstance().claimTownChunk(chunkPosition, townData.getID());
+
+        Player player = Factory.getRandomPlayer();
+        when(player.getLocation()).thenReturn(new Location(world, 0, 0, 0));
+        when(world.getChunkAt(any(Location.class))).thenReturn(otherChunkPosition);
+
+        UnclaimAdminCommand unclaim = new UnclaimAdminCommand();
+        unclaim.perform(player, new String[]{"unclaim"});
+
+        assertTrue(NewClaimedChunkStorage.getInstance().isChunkClaimed(chunkPosition));
     }
 }
