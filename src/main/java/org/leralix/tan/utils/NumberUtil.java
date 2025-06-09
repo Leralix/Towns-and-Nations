756201f5c02b9dd1741a277b4273243ae01c1f1e
Leralix <78090544+Leralix@users.noreply.github.com>
2025-06-09 02:14:35 +0200

    Cleaning for 2.14.5 + Lang update

--- src/main/java/org/leralix/tan/utils/NumberUtil.java
+++ src/main/java/org/leralix/tan/utils/NumberUtil.java
@@ -13,9 +13,6 @@ public class NumberUtil {
     }
 
     public static double roundWithDigits(double value){
-        System.out.println(value);
-        System.out.println("nbDigits : " + nbDigits);
-        System.out.println("res : " + Math.round(value * Math.pow(10, nbDigits)) / Math.pow(10, nbDigits));
         return Math.round(value * Math.pow(10, nbDigits)) / Math.pow(10, nbDigits);
     }
 }
