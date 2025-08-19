1b8ec9779dca4c4dc7320ee562d7754e39653210
Leralix <78090544+Leralix@users.noreply.github.com>
2025-08-19 20:35:50 +0200

    Fixing reversed iterator buttons

--- src/main/java/org/leralix/tan/gui/IteratorGUI.java
+++ src/main/java/org/leralix/tan/gui/IteratorGUI.java
@@ -24,12 +24,12 @@ public abstract class IteratorGUI extends BasicGui {
     }
 
 
-    protected void iterator(List<GuiItem> itemList, Consumer<Player> onLeave){
+    protected void iterator(List<GuiItem> itemList, Consumer<Player> onLeave) {
         iterator(itemList, onLeave, Material.GRAY_STAINED_GLASS_PANE);
     }
 
-    protected void iterator(List<GuiItem> itemList, Consumer<Player> onLeave, Material decorativeMaterial){
-        GuiUtil.createIterator(gui, itemList, page, player, onLeave, p -> previousPage(), p -> nextPage(), decorativeMaterial);
+    protected void iterator(List<GuiItem> itemList, Consumer<Player> onLeave, Material decorativeMaterial) {
+        GuiUtil.createIterator(gui, itemList, page, player, onLeave, p -> nextPage(), p -> previousPage(), decorativeMaterial);
     }
 
     protected void previousPage() {
