c4edbf0df4a74d4cb2f16e382b119ed6cbddee1a
Leralix <78090544+Leralix@users.noreply.github.com>
2025-09-23 01:45:18 +0200

    Register DAO for landmarks events

--- src/main/java/org/leralix/tan/events/newsletter/dao/NewsletterDAO.java
+++ src/main/java/org/leralix/tan/events/newsletter/dao/NewsletterDAO.java
@@ -34,6 +34,8 @@ public class NewsletterDAO {
         subDaos.put(NewsletterType.TERRITORY_VASSAL_INDEPENDENT, new TerritoryVassalIndependentDAO(dataSource));
         subDaos.put(NewsletterType.DIPLOMACY_ACCEPTED, new DiplomacyAcceptedDAO(dataSource));
         subDaos.put(NewsletterType.DIPLOMACY_PROPOSAL, new DiplomacyProposalDAO(dataSource));
+        subDaos.put(NewsletterType.LANDMARK_CLAIMED, new LandmarkClaimedDAO(dataSource));
+        subDaos.put(NewsletterType.LANDMARK_UNCLAIMED, new LandmarkUnclaimedDAO(dataSource));
         subDaos.put(NewsletterType.ATTACK_DECLARED, new AttackDeclaredDAO(dataSource));
         subDaos.put(NewsletterType.ATTACK_WON_BY_ATTACKER, new AttackWonByAttackerDAO(dataSource));
         subDaos.put(NewsletterType.ATTACK_WON_BY_DEFENDER, new AttackWonByDefenderDAO(dataSource));
