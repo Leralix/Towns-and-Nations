eb9744a850dd8a3963aaaa8b3a1b9fab20b90a6e
Leralix <78090544+Leralix@users.noreply.github.com>
2025-11-25 22:57:02 +0100

    Custom icons for GeneralChunkSetting

--- src/main/java/org/leralix/tan/enums/permissions/GeneralChunkSetting.java
+++ src/main/java/org/leralix/tan/enums/permissions/GeneralChunkSetting.java
@@ -1,6 +1,6 @@
 package org.leralix.tan.enums.permissions;
 
-import org.bukkit.Material;
+import org.leralix.tan.gui.cosmetic.IconKey;
 import org.leralix.tan.gui.cosmetic.IconManager;
 import org.leralix.tan.gui.cosmetic.type.IconBuilder;
 import org.leralix.tan.lang.Lang;
@@ -9,22 +9,22 @@ import org.leralix.tan.utils.constants.Constants;
 import org.leralix.tan.war.legacy.InteractionStatus;
 
 public enum GeneralChunkSetting {
-    ENABLE_PVP(Material.DIAMOND_SWORD, Lang.ENABLE_PVP_SETTING),
-    FIRE_GRIEF(Material.FLINT_AND_STEEL, Lang.ENABLE_FIRE_GRIEF_SETTING),
-    TNT_GRIEF(Material.TNT, Lang.ENABLE_TNT_GRIEF_SETTING),
-    MOB_GRIEF(Material.ENDER_PEARL, Lang.ENABLE_MOB_GRIEF_SETTING);
+    ENABLE_PVP(IconKey.ENABLE_PVP_ICON, Lang.ENABLE_PVP_SETTING),
+    FIRE_GRIEF(IconKey.ENABLE_FIRE_GRIEF_ICON, Lang.ENABLE_FIRE_GRIEF_SETTING),
+    TNT_GRIEF(IconKey.ENABLE_TNT_GRIEF_ICON, Lang.ENABLE_TNT_GRIEF_SETTING),
+    MOB_GRIEF(IconKey.ENABLE_MOB_GRIEF_ICON, Lang.ENABLE_MOB_GRIEF_SETTING);
 
-    private final Material material;
+    private final IconKey iconKey;
     private final Lang name;
 
-    GeneralChunkSetting(Material icon, Lang name) {
-        this.material = icon;
+    GeneralChunkSetting(IconKey iconKey, Lang name) {
+        this.iconKey = iconKey;
         this.name = name;
     }
 
     public IconBuilder getIcon(IconManager iconManager, boolean isEnabled, LangType lang) {
 
-        IconBuilder iconBuilder = iconManager.get(this.material)
+        IconBuilder iconBuilder = iconManager.get(this.iconKey)
                 .setName(name.get(lang));
 
         InteractionStatus state = Constants.getChunkSettings(this);
