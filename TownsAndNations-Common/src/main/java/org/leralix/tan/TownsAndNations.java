34585f96a3822c3ba5c158be6668ccb42c306afc
Leralix <78090544+Leralix@users.noreply.github.com>
2026-01-01 22:47:17 +0100

    v0.16.7

--- TownsAndNations-Common/src/main/java/org/leralix/tan/TownsAndNations.java
+++ TownsAndNations-Common/src/main/java/org/leralix/tan/TownsAndNations.java
@@ -84,7 +84,7 @@ public class TownsAndNations extends JavaPlugin {
      * Used to check if the plugin is up-to-date to the latest version. Also
      * used to check if the plugin has just been updated and config file needs an update
      */
-    private static final PluginVersion CURRENT_VERSION = new PluginVersion(0, 16, 6);
+    private static final PluginVersion CURRENT_VERSION = new PluginVersion(0, 16, 7);
 
     private static final PluginVersion MINIMUM_SUPPORTING_DYNMAP = new PluginVersion(0, 15, 0);
 
