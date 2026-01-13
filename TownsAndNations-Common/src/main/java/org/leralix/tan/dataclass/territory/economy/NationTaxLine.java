35213179acf25df69617e2ffbe8f36af7a7f7c89
Dorukium <dorukersoytwitch@gmail.com>
2026-01-13 22:27:26 +0100

    Create TaxProfitLine base class to eliminate tax line duplication
    - New TaxProfitLine extends ProfitLine with common tax GUI logic
    
    - NationTaxLine and SubjectTaxLine now only contain constructor-specific logic
    
    - Reduces duplication from 55.6%/100% to near 0%

--- TownsAndNations-Common/src/main/java/org/leralix/tan/dataclass/territory/economy/NationTaxLine.java
+++ TownsAndNations-Common/src/main/java/org/leralix/tan/dataclass/territory/economy/NationTaxLine.java
@@ -1,18 +1,9 @@
 package org.leralix.tan.dataclass.territory.economy;
 
-import dev.triumphteam.gui.guis.Gui;
-import org.bukkit.entity.Player;
 import org.leralix.tan.dataclass.territory.NationData;
 import org.leralix.tan.dataclass.territory.TerritoryData;
-import org.leralix.tan.lang.FilledLang;
-import org.leralix.tan.lang.Lang;
-import org.leralix.tan.lang.LangType;
-import org.leralix.tan.utils.text.StringUtil;
 
-public class NationTaxLine extends ProfitLine {
-
-    double actualTaxes = 0;
-    double missingTaxes = 0;
+public class NationTaxLine extends TaxProfitLine {
 
     public NationTaxLine(NationData nationData) {
         super(nationData);
@@ -23,31 +14,5 @@ public class NationTaxLine extends ProfitLine {
             else
                 missingTaxes += tax;
         }
-    }
-
-    @Override
-    protected double getMoney() {
-        return actualTaxes;
-    }
-
-    @Override
-    public FilledLang getLine() {
-        if (missingTaxes > 0)
-            return Lang.PLAYER_TAX_MISSING_LINE.get(StringUtil.getColoredMoney(getMoney()), Double.toString(missingTaxes));
-        else
-            return Lang.PLAYER_TAX_LINE.get(StringUtil.getColoredMoney(getMoney()));
-    }
-
-    @Override
-    public void addItems(Gui gui, Player player, LangType lang) {
-        double taxRate = territoryData.getTax();
-        addDecreaseItem(gui, player, lang, taxRate);
-        addInfoItem(gui, player, lang, taxRate);
-        addIncreaseItem(gui, player, lang);
-    }
-
-    @Override
-    public boolean isRecurrent() {
-        return true;
     }
 }
