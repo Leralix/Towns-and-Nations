4e953e64b2267ff85fb28e80d1fc7de62321408b
Dorukium <dorukersoytwitch@gmail.com>
2026-01-13 22:02:23 +0100

    Move AbstractTributeDailyStorage to its own file (CodeFactor Checkstyle)
    - Satisfies OneTopLevelClassCheck: top-level class now resides in its own source file

--- TownsAndNations-Common/src/main/java/org/leralix/tan/storage/stored/TributePlayerDailyStorage.java
+++ TownsAndNations-Common/src/main/java/org/leralix/tan/storage/stored/TributePlayerDailyStorage.java
@@ -1,36 +1,5 @@
 package org.leralix.tan.storage.stored;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.GsonBuilder;
-
-import java.util.LinkedHashMap;
-
-abstract class AbstractTributeDailyStorage extends JsonStorage<Double> {
-
-    protected AbstractTributeDailyStorage(String fileName) {
-        super(
-                fileName,
-                new TypeToken<LinkedHashMap<String, Double>>() {}.getType(),
-                new GsonBuilder().setPrettyPrinting().create()
-        );
-    }
-
-    public synchronized double getAmount(String id) {
-        Double val = dataMap.get(id);
-        return val == null ? 0.0 : val;
-    }
-
-    public synchronized void addAmount(String id, double amount) {
-        dataMap.put(id, getAmount(id) + amount);
-        save();
-    }
-
-    public synchronized void resetDaily() {
-        dataMap.clear();
-        save();
-    }
-}
-
 public class TributePlayerDailyStorage extends AbstractTributeDailyStorage {
 
     private static TributePlayerDailyStorage instance;
