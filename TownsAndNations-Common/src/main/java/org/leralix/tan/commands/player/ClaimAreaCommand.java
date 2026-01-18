efdc5ff65b8ed03c52f1677202c2a801138fe455
Dorukium <118289068+Dorukium@users.noreply.github.com>
2026-01-18 23:05:17 +0100

    Sync permissions (#98)

--- TownsAndNations-Common/src/main/java/org/leralix/tan/commands/player/ClaimAreaCommand.java
+++ TownsAndNations-Common/src/main/java/org/leralix/tan/commands/player/ClaimAreaCommand.java
@@ -29,7 +29,7 @@ public class ClaimAreaCommand extends AbstractTerritoryClaimCommand {
 
     @Override
     public String getSyntax() {
-        return "/tan claimarea <town/region/nation>";
+        return "/tan claimarea <town/region/nation|kingdom>";
     }
 
     @Override
