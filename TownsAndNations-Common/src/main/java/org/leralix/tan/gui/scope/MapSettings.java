3f671f6103f214f429596921a18ca926c633d772
. <251542432+rsselsch@users.noreply.github.com>
2026-05-19 19:06:34 -0700

    Update MapSettings.java

--- TownsAndNations-Common/src/main/java/org/leralix/tan/gui/scope/MapSettings.java
+++ TownsAndNations-Common/src/main/java/org/leralix/tan/gui/scope/MapSettings.java
@@ -35,8 +35,7 @@ public class MapSettings {
 
     public Component getMapTypeButton(LangType langType){
         return Component.text(claimType.getName(langType))
-                .hoverEvent(HoverEvent.showText(Component.text(Lang.LEFT_CLICK_TO_MODIFY.get(langType))))
-                .clickEvent();
+                .hoverEvent(HoverEvent.showText(Component.text(Lang.LEFT_CLICK_TO_MODIFY.get(langType))));
                 //.clickEvent(ClickEvent.clickEvent(
                 //        ClickEvent.Action.RUN_COMMAND,
                 //        ClickEvent.Payload.string(getNextCommand(claimAction, claimType.getNextType()))
