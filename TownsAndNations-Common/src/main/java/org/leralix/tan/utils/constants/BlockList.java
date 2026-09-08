0e4daa1617129b3c7bb92f731f20951b3a545f11
Leralix <78090544+Leralix@users.noreply.github.com>
2026-09-08 19:56:40 +0200

    Better fort flag protection against griefing

---
+++ TownsAndNations-Common/src/main/java/org/leralix/tan/utils/constants/BlockList.java
@@ -0,0 +1,28 @@
+package org.leralix.tan.utils.constants;
+
+import org.bukkit.Material;
+
+import java.util.List;
+
+public class BlockList {
+
+    public static final List<Material> BREAKABLE_BLOCKS = List.of(
+            Material.PISTON_HEAD,
+            Material.PISTON,
+            Material.MOVING_PISTON,
+            Material.STICKY_PISTON,
+            Material.BAMBOO,
+            Material.SUGAR_CANE,
+            Material.WHEAT,
+            Material.POTATO,
+            Material.BEETROOT_SEEDS,
+            Material.MELON_SEEDS,
+            Material.PUMPKIN_SEEDS,
+            Material.WHEAT_SEEDS,
+            Material.SCAFFOLDING,
+            Material.ICE,
+            Material.BLUE_ICE,
+            Material.PACKED_ICE,
+            Material.FROSTED_ICE
+    );
+}
